0980b040ad4f4c64b0c7e013aa59dbc0029cb606
Judson Bandeira <judsonmelobandeira@gmail.com>
2023-04-29 10:31:07 -0300

    Creating migrations. Still waiting the Adeildo's answer about the users table in diagram.

---
+++ database/migrations/2023_04_29_132112_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('products');
+            $table->foreignId('merchant_id')->constrained();
+            $table->integer('price');
+            $table->enum('status', ['out_of_stock', 'in_stock', 'running_low']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
